06ae57000e80a0712e9e06d5dd75075607b1c8ae
Marterich <m-wiethan@gmx.de>
2021-12-06 23:59:29 +0100

    day06 solution

---
+++ 2021/day06/solve.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3 
+raw = [int(n) for n in [x.split(",") for x in open("input.txt","r")][0]]
+def age(days):
+	fish_ages = 10*[0]
+	for f in raw:
+		fish_ages[f] += 1
+	for d in range(days):
+		for i in range(len(fish_ages)):
+			if i == 0:
+				fish_ages[8+1] += fish_ages[0]
+				fish_ages[6+1] += fish_ages[0]
+				fish_ages[0] = 0
+			else:
+				fish_ages[i-1] = fish_ages[i]
+				fish_ages[i] = 0
+	return (sum(fish_ages))
+print("Part1: %d" % age(80))
+print("Part2: %d" % age(256))
